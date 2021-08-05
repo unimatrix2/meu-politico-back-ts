6d8b25b690a9eca4017f31867244113e939716be
unimatrix2 <dnny.nis@gmail.com>
2021-08-05 16:36:33 -0300

    added stack to AppError

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -2,12 +2,15 @@ export default class AppError extends Error {
     message: string;
     type: string;
     status: number;
+    stack: any;
 
-    constructor(message: string, type: string, status: number) {
+    constructor(message: string,
+        type: string, status: number, stack?: any) {
         super(message);
 
         this.message = message || 'Ops! Ocorreu um erro. Tente novamente mais tarde.';
         this.type = type || 'Erro no Servidor';
         this.status = status || 500;
+        this.stack = stack;
     }
 }
