ae8574f8307a9a843f4c0ed931c3f28753137164
unimatrix2 <dnny.nis@gmail.com>
2021-08-07 00:58:18 -0300

    added router hub file

---
+++ src/routes/router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+// Import Routes & Auth Middleware here
+import authRoutes from './routes/auth.routes';
+// import noticiaRoutes from './noticiaRoutes/noticia.routes';
+// import politicoRoutes from './politicoRoutes/politico.routes';
+
+// Config router & routes
+const router = Router();
+
+router.use('/usuario', authRoutes);
+// router.use('/noticias', noticiaRoutes);
+// router.use('/politicos', politicoRoutes); 
+
+export default router;
