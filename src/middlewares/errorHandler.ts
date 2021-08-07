75ef6488acb82ee205bbde63918a65a784aac16d
unimatrix2 <dnny.nis@gmail.com>
2021-08-07 01:06:21 -0300

    removed unused middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,14 +1,6 @@
 import { Request, Response } from 'express'
 import AppError from '../errors/AppError';
 
-export const handleError = (err: AppError, req: Request, res: Response) => {
-	console.log(err);
-	const { message, type, status } = err;
-	const statusCode = status || 500;
-
-	return res.status(statusCode).json({ message, type: type || '', status: statusCode });
-}
-
 export const handle404 = (req: Request, res: Response) => {
     res.status(404).json({
         name: 'Page404',
