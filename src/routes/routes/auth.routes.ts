b3e361310e458ffd5c676109db201e2b9e2f723b
unimatrix2 <dnny.nis@gmail.com>
2021-08-07 22:18:14 -0300

    fixed routes

--- src/routes/routes/auth.routes.ts
+++ src/routes/routes/auth.routes.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Response, Router } from 'express';
 import {validateSignupParams, validateLoginParams} from '../../middlewares/authValidation';
-import { register, authenticateUser, provideLoggedUserInfo,  } from '../../services/auth.service';
-import AppError from '../../errors/AppError';
+import { register, authenticateUser, provideNewSessionInfo  } from '../../services/auth.service';
 import { routeProtection } from '../../middlewares/routeProtection';
+import { newCredentials } from '../../interfaces/user';
 
 const router = Router();
 
@@ -26,18 +26,29 @@ router.post('/acesso', validateLoginParams, async (req: any, res: Response, next
             signed: true,
             httpOnly: true,
             sameSite: true,
-            maxAge: process.env.COOKIE_EXPIRY,
+            maxAge: process.env.COOKIE_EXPIRY
         }).status(200).json({ message: 'Sucesso!' });
-    } catch (error) { return next(new AppError(error)) };
+    } catch (error) {
+        res.status(error.status).json(error);
+    };
 });
 
 router.use(routeProtection);
 
 router.get('/token', async (req: any, res: Response, nxt: NextFunction) => {
     try {
-        const user = await provideLoggedUserInfo(req.user.id);
-        res.status(200).json(user);
-    } catch (error) { return nxt(new AppError(error)) }
+        const user: newCredentials = await provideNewSessionInfo(req.user.id);
+        res.status(200).cookie(
+            'token', user.newToken, {
+                secure: true,
+                signed: true,
+                httpOnly: true,
+                sameSite: true,
+                maxAge: process.env.COOKIE_EXPIRY
+            }).json(user.userData);
+    } catch (error) {
+        res.status(error.status).clearCookie('token').json(error);
+    }
 })
 
 // ESSA ROTA SERÁ RETRABALHADA PARA PROVER SOMENTE ATUALIZAÇÃO DE INFO
