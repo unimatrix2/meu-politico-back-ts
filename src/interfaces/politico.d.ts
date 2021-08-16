e4ddec72f786be11dadef5e36f33aaf1fc0edabf
Danny Nisenbaum <dnny.nis@gmail.com>
2021-08-15 23:57:43 -0300

    added create interfaces

---
+++ src/interfaces/politico.d.ts
@@ -0,0 +1,19 @@
+interface historyBody {
+    position: string;
+    period: {
+        begin: Date;
+        end: Date;
+    }
+}
+
+export interface politicoCreateBody {
+    fullName: string;
+    socialName: string;
+    currentPosition: string;
+    history?: historyBody[];
+    status: string;
+    province: string;
+    owner: string;
+    officialInfoURL: string;
+    imageURL: string;
+}
