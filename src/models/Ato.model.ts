2fe06110bb69317eabb61124295a107345c46a1b
unimatrix2 <dnny.nis@gmail.com>
2021-08-25 00:19:36 -0300

    added ato model

--- src/models/Ato.model.ts
+++ src/models/Ato.model.ts
@@ -4,7 +4,8 @@ import { Schema, model } from 'mongoose';
 const AtoSchema = new Schema({
     descricao: { type: String, required: true },
     fonte_oficial: { type: String, required: true },
-    politicos: [{ type: Schema.Types.ObjectId, ref: 'Politico', required: true }]
+    politicos: [{ type: Schema.Types.ObjectId, ref: 'Politico', required: true }],
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 }, { timestamps: true });
 
 const Ato = model('Act', AtoSchema);
