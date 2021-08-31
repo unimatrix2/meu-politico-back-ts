fdf5c3b2185b53af2601430d4e1781ff3886c509
unimatrix2 <dnny.nis@gmail.com>
2021-08-30 23:05:20 -0300

    fixed route protection

--- src/middlewares/routeProtection.ts
+++ src/middlewares/routeProtection.ts
@@ -3,27 +3,22 @@ import AppError from '../errors/AppError';
 import { verify } from '../utils/tokenManager';
 
 export const routeProtection = (req: any, res: Response, nxt: NextFunction) => {
-    const { token } = req.signedCookies;
-
-    if (!token) {
-        throw new AppError({
-            message: 'Não há credenciais de acesso, ou as credenciais são inválidas',
-            type: 'Acesso-Credencial-Invalida-Ou-Inexistente',
-            status: 401
-        });
-    }
-
-    let decodedToken;
-
-    try {
-        decodedToken = verify(token);
-        req.user = { id: decodedToken.id };
-    } catch (error) {
-        throw new AppError({
-            message: 'Acesso expirado',
-            type: 'Acesso-Expirado',
-            status: 401
-        });
-    }
-    return nxt();
+	try {
+		const { token } = req.signedCookies;
+		if (!token) {
+			throw new AppError({
+				message: 'Não há credenciais de acesso',
+				type: 'Acesso-Credencial-Inexistente',
+				status: 401
+			});
+		}
+		const decodedToken = verify(token);
+		req.user = { id: decodedToken.id };
+		return nxt();
+	} catch (error: any) {
+		res
+			.clearCookie('token')
+			.status(error.status)
+			.json(new AppError(error));
+	}
 }
