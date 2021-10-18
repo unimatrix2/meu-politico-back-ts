11e482ef1b79e92b1485441ae0bd775e3b7e7a21
unimatrix2 <dnny.nis@gmail.com>
2021-10-17 22:34:31 -0300

    first fixes & began documenting the server

--- src/models/Ato.model.ts
+++ src/models/Ato.model.ts
@@ -1,9 +1,24 @@
-import { timeStamp } from 'console';
+/**
+ * @module AtoModel
+ * @description Esse módulo contém o modelo de Ato oficial para um político.
+ * Atualmente o ato é um modelo de relação independente da hierarquia de dados,
+ * e contém uma array de políticos envolvidos no ato. Idealmente essa array será usada
+ * de forma limitada para identificar autoria e co-autoria.
+ */
 import { Schema, model } from 'mongoose';
 
+/**
+ * Definição do schema de ato
+ * @param {string} description
+ * @param {string} source
+ * @param {array} news
+ * @param {array} politicos
+ * @param {string} owner
+ */
 const AtoSchema = new Schema({
-    descricao: { type: String, required: true },
-    fonte_oficial: { type: String, required: true },
+    description: { type: String, required: true },
+    source: { type: String, required: true },
+    news: [{ type: Schema.Types.ObjectId, ref: 'News' }],
     politicos: [{ type: Schema.Types.ObjectId, ref: 'Politico', required: true }],
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 }, { timestamps: true });
