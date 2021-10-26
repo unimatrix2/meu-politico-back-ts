329c67898f495d828a2c4c849a14c8f887953658
unimatrix2 <dnny.nis@gmail.com>
2021-10-25 22:06:33 -0300

    added enum array for election schema

---
+++ src/constants/elections.enum.ts
@@ -0,0 +1,11 @@
+import { IElectionTypeEnum } from '../interfaces/enum';
+
+const electionEnum: IElectionTypeEnum[] = [
+    IElectionTypeEnum.ORDN,
+    IElectionTypeEnum.ORDM,
+    IElectionTypeEnum.SUPL,
+    IElectionTypeEnum.PLEB,
+    IElectionTypeEnum.REFE
+];
+
+export default electionEnum;
