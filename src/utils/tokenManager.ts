5f4885ec6acfc454b9701709ce02b3704afdc0b9
unimatrix2 <dnny.nis@gmail.com>
2021-11-02 22:36:48 -0300

    prettified

--- src/utils/tokenManager.ts
+++ src/utils/tokenManager.ts
@@ -2,23 +2,21 @@ import jwt from 'jsonwebtoken';
 import AppError from '../errors/AppError';
 
 export const verify = (token: string): any => {
-    try {
-        const verified =  jwt.verify(token, process.env.TOKEN_SECRET);
-        return verified;
-    } catch (error) {
-        throw new AppError({
-            message: 'Token Inválido',
-            type: 'Acesso-Token-Inválido',
-            status: 401
-        });
-    }
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		return verified;
+	} catch (error) {
+		throw new AppError({
+			message: 'Token Inválido',
+			type: 'Acesso-Token-Inválido',
+			status: 401,
+		});
+	}
 };
 
 export const generate = (id: string): string => {
-    const token = jwt.sign(
-        { id },
-        process.env.TOKEN_SECRET,
-        { expiresIn: process.env.TOKEN_EXPIRATION }
-    );
-    return token;
-}
+	const token = jwt.sign({ id }, process.env.TOKEN_SECRET, {
+		expiresIn: process.env.TOKEN_EXPIRATION,
+	});
+	return token;
+};
