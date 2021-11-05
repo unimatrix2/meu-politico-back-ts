3973c50f9d35a56c0119d375cb6a94d92b539f6e
unimatrix2 <dnny.nis@gmail.com>
2021-11-05 00:45:50 -0300

    now mapping multiple errors

--- src/mappers/userCreateError.mapper.ts
+++ src/mappers/userCreateError.mapper.ts
@@ -1,7 +1,12 @@
 const userCreateErrorMapper = (error: any) => {
-    let errorKeys = Object.keys(error.keyValue)[0].split('');
-    errorKeys[0] = errorKeys[0].toUpperCase();
-    return errorKeys.join('');
-}
+	const errorKeys = Object.keys(error.keyValue).map((value: string) => {
+		const wordArray = value.split('');	// se concatenar aqui, será o retorno do splice
+		wordArray														// e não queremos isso.
+			.splice(0, 1, value.split('')[0].toUpperCase());
+		// Concatenar o join também atuaria no retorno do splice, e não queremos isso.
+		return [...wordArray].join('');
+	});
+	return errorKeys.join('-');
+};
 
 export default userCreateErrorMapper;
